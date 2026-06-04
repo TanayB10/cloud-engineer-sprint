0a48892906c9886ebf0648a49683caf0ac105ee3
Tanay Balakrishna <tanaybalakrishna26@gmail.com>
2026-06-04 21:57:20 +0530

    Deploys automated GitHub Actions CI pipeline framework

--- terraform-labs/week18/pipeline_test.tf
+++ terraform-labs/week18/pipeline_test.tf
@@ -19,6 +19,6 @@ resource "aws_security_group" "pipeline_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = var.missing_variable_typo # Intentional Configuration Error!
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
